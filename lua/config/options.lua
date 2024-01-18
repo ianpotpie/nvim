e7ea0fec656416ac746092087ba2cd0c2c16b012
Ian Gordon <ianbgordon@gmail.com>
2024-01-17 19:54:16 -0500

    new options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,16 @@
 -- set the colorscheme
-vim.cmd([[colorscheme onedark]])
+vim.cmd.colorscheme('onedark')
 
 -- set formatting behavior
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
 vim.opt.relativenumber = false
 vim.opt.number = true
 vim.opt.wrap = false
+vim.opt.linebreak = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+
+-- set data-saving behavior
+vim.opt.undofile = true
 vim.opt.clipboard = 'unnamedplus'
 
