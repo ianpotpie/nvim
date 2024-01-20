a936e43df044a86533ee5561166f0c07ab6419f5
Ian Gordon <ianbgordon@gmail.com>
2024-01-20 00:12:39 -0500

    set conform to format on save and format on <leader>mp

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -20,6 +20,19 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
       },
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
     })
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
   end,
 }
