83904b3786c727ba176be8d03ab157884c3077d8
Ian Gordon <ianbgordon@gmail.com>
2024-01-20 00:20:28 -0500

    update telescope config to change cwd and show hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,35 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
--- or                              , branch = '0.1.x',
-      dependencies = { 
-	      'nvim-lua/plenary.nvim',
-	      'BurntSushi/ripgrep',
-	      'sharkdp/fd',
-	      'nvim-tree/nvim-web-devicons'
-      }
-}
+	'nvim-telescope/telescope.nvim',
+	branch = '0.1.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'BurntSushi/ripgrep',
+		'sharkdp/fd',
+		'nvim-tree/nvim-web-devicons'
+	},
+	config = function()
+		local telescope = require('telescope')
+		local actions = require('telescope.actions')
 
+		telescope.setup{
+			defaults = {
+				mappings = {
+					i = {
+						["<esc>"] = actions.close
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+					theme = "dropdown",
+					cwd = vim.fn.getcwd(), -- This sets the working directory to the current directory
+				},
+			},
+			extensions = {
+				-- Your extensions configuration here
+			},
+		}
+	end
+}
 
