3c3b7b08452b3643da1bdc75bc7c29a667aab7fd
Ian Gordon <ianbgordon@gmail.com>
2024-01-27 15:42:19 -0500

    add keymap descriptions and new keymap that deletes buffer while keeping window open

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 -- setting the leader key
 map("", "<Space>", "<Nop>")
@@ -6,28 +7,52 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Resize with arrows
-map("n", "<C-Up>", ":resize -2<CR>")
-map("n", "<C-Down>", ":resize +2<CR>")
-map("n", "<C-Left>", ":vertical resize -2<CR>")
-map("n", "<C-Right>", ":vertical resize +2<CR>")
+opts.desc = "Increase window width"
+map("n", "<C-Up>", ":resize +2<CR>", opts)
+
+opts.desc = "Decrease window width"
+map("n", "<C-Down>", ":resize -2<CR>", opts)
+
+opts.desc = "Decrease window height"
+map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+
+opts.desc = "Increase window height"
+map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-map("n", "<S-l>", ":bnext<CR>")
-map("n", "<S-h>", ":bprevious<CR>")
+opts.desc = "Switch to next buffer"
+map("n", "<S-l>", ":bnext<CR>", opts)
+
+opts.desc = "Switch to prev buffer"
+map("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Remap for dealing with word wrap
+opts.desc = "Move up a line (within wrap)"
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+opts.desc = "Move down a line (within wrap)"
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Escape to clear highlights
-map("n", "<ESC>", ":noh<CR>")
+opts.desc = "Pass escape key and remove highlights"
+map("n", "<ESC>", ":noh<CR>", opts)
 
 -- general keymapping
-map("i", "jk", "<ESC>")
-map("n", "<leader>term", ":term<CR>i")
-map("n", "<leader>wrap", ":set wrap!<CR>")
-map("n", "<leader>spell", ":set spell!<CR>")
-map("n", "<leader>relative", ":set relativenumber!<CR>")
+opts.desc = "Open terminal"
+map("n", "<leader>term", ":term<CR>i", opts)
+
+opts.desc = "Toggle line wrap"
+map("n", "<leader>wrap", ":set wrap!<CR>", opts)
+
+opts.desc = "Toggle the vim builtin spell-checker"
+map("n", "<leader>spell", ":set spell!<CR>", opts)
+
+opts.desc = "Toggle relative line numbering"
+map("n", "<leader>relative", ":set relativenumber!<CR>", opts)
+
+opts.desc = "Delete the buffer while keeping the window open"
+map("n", "<leader>bd", ":bnext<CR>:bdelete #<CR>", opts)
 
 -- quickly exit insert mode in the terminal
-map("t", "<ESC>", "<C-\\><C-N>")
+opts.desc = "Exit the terminal with <ESC>"
+map("t", "<ESC>", "<ESC><C-\\><C-N>", opts)
